8513f46199eda520219883967a412ba188e78366
Yuliia Suprun <ys70@agate.clear.rice.edu>
2022-04-10 00:15:10 -0500

    Programs for procceses and kernel.h

--- init.c
+++ init.c
@@ -1,9 +1,10 @@
 #include <comp421/hardware.h>
+#include <comp421/yalnix.h>
 #include <stdlib.h>
 #include <string.h>
 int x = 5;
 int y = 34;
-char s2[10] = "Hello";
+char s2[10] = "Help!";
 int main() {
     while(1) {
         TracePrintf(0, "Init is working\n");
@@ -12,6 +13,7 @@ int main() {
         char *s = malloc(sizeof(char)*5);
         strcpy(s, s2);
         TracePrintf(0, "Malloced string: %s\n", s);
+        TracePrintf(0, "PID of the current process: %i\n", GetPid());
         Pause();
     }
     return 0;
--- kernel.h
+++ kernel.h
@@ -7,18 +7,27 @@ struct physical_frame {
     struct physical_frame *next;
 };
 
-typedef struct pcb {
-    pid_t pid;
+typedef struct queue queue;
+typedef struct pcb pcb;
+struct pcb {
+    int pid;
     SavedContext ctx;
     struct pte *page_table0;
     int delay_clock;
     struct pcb *parent;
     struct pcb *next;
-    // unsigned long brk;
-    // int n_child;
-    // StatusQueue *statusQ;
-} pcb;
+    void *brk;
+    int n_child;
+    queue *statusQ;
+    int status;
+};
+struct queue {
+    pcb *head;
+    pcb *tail;
+    int count;
+};
 
-int LoadProgram(char *name, char **args, ExceptionInfo* info, struct pte* region0, struct free_pages free_pages);
-void freePage(struct pte* newPte);
+
+int LoadProgram(char *name, char **args, ExceptionInfo *info, struct pte *region0, struct free_pages free_pages, pcb *newPCB);
+void freePage(struct pte *newPte);
 unsigned int getFreePage(); 
